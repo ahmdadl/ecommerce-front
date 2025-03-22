5e8f34d86f88b3fc46afd31f828e86717547a79c
abo3adel <abo3adel35@gmail.com>
2025-03-22 16:36:17 +0200

    feat: add env variables

--- src/modules/core/utils/env.ts
+++ src/modules/core/utils/env.ts
@@ -2,4 +2,5 @@ export const env = {
     environment: import.meta.env.VITE_ENVIRONMENT,
     isDev: import.meta.env.VITE_ENVIRONMENT === 'development',
     apiUrl: import.meta.env.VITE_API_URL,
+    apiKey: import.meta.env.VITE_API_KEY,
 };
