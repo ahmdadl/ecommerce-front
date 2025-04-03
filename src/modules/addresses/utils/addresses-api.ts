40573d12375fc22183a4d9cfcebf7e34bcdac6c3
abo3adel <abo3adel35@gmail.com>
2025-04-03 21:16:32 +0200

    feat(addresses): add skelton loading pag

--- src/modules/addresses/utils/addresses-api.ts
+++ src/modules/addresses/utils/addresses-api.ts
@@ -1,6 +1,10 @@
 import http from '@/modules/core/utils/http';
 
 export const addressApi = {
+    getAll: (withGovernments: boolean = false, withCities: boolean = false) =>
+        http.get('/addresses', {
+            params: { withGovernments, withCities },
+        }),
     create: (data: any) => http.post('/addresses', data),
     update: (addressId: string, data: any) =>
         http.patch('/addresses/' + addressId, data),
--- src/routes/$locale/profile/addresses.tsx
+++ src/routes/$locale/profile/addresses.tsx
@@ -1,6 +1,27 @@
 import AddressesPage from '@/modules/addresses/pages/AddressesPage';
+import AddressesSkeletonPage from '@/modules/addresses/pages/AddressesSkeletonPage';
+import { addressesStore } from '@/modules/addresses/stores/addresses-store';
+import { addressApi } from '@/modules/addresses/utils/addresses-api';
+import { parseError } from '@/modules/core/utils/parseError';
 import { createFileRoute } from '@tanstack/react-router';
+import { AxiosResponse } from 'axios';
 
 export const Route = createFileRoute('/$locale/profile/addresses')({
     component: AddressesPage,
+    loader: async () => {
+        const response = (await addressApi
+            .getAll(true, true)
+            .catch(parseError)) as AxiosResponse;
+
+        if (!response?.data?.data?.addresses) return;
+
+        const data = response.data.data;
+
+        addressesStore.setState({
+            list: data.addresses,
+            governments: data.governments,
+            cities: data.cities,
+        });
+    },
+    pendingComponent: AddressesSkeletonPage,
 });
