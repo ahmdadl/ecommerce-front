11def6ce94101b565b9f99e86c2f746400f5d601
abo3adel <abo3adel35@gmail.com>
2025-04-05 21:36:25 +0200

    feat(categories): finish page

--- src/routes/$locale/_catalog/categories/index.tsx
+++ src/routes/$locale/_catalog/categories/index.tsx
@@ -1,10 +1,12 @@
 import CategoriesPage from '@/modules/categories/pages/CategoriesPage';
+import { CategorySkeletonPage } from '@/modules/categories/pages/CategoriesSkeletonPage';
+import { categoriesApi } from '@/modules/categories/utils/categories-api';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/$locale/_catalog/categories/')({
     component: CategoriesPage,
-});
 
-// function RouteComponent() {
-//     return <div>Hello "/$locale/_catalog/categories/"!</div>;
-// }
+    loader: () => categoriesApi.load(),
+
+    pendingComponent: CategorySkeletonPage,
+});
