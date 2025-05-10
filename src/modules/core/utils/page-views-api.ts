b9b7bd2bfc868efe2850508aba4e022b3a529d59
abo3adel <abo3adel35@gmail.com>
2025-05-10 23:37:17 +0300

    feat: deploy

--- src/modules/core/utils/page-views-api.ts
+++ src/modules/core/utils/page-views-api.ts
@@ -3,7 +3,7 @@ import { ViewableType } from './types';
 
 export const pageViewsApi = {
     store: ({ type, slug }: { type: ViewableType; slug: string }) => {
-        http.post<void>(`/page-views/`, {
+        http.post<void>(`/page-views`, {
             type,
             slug,
             user_agent: navigator.userAgent,
